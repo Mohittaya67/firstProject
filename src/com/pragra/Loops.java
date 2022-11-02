aa5f7ca8de0e6a7063ab7c9533329998bea4f6ce
mohittaya67 <Mohittaya67@gmail.com>
2022-11-01 20:41:38 -0400

    newChange

--- src/com/pragra/Loops.java
+++ src/com/pragra/Loops.java
@@ -2,10 +2,11 @@ package com.pragra;
 
 public class Loops {
     public static void main(String[] args) {                        //for loops
-        for (int mohit = 2; mohit <= 5; mohit++) {
-            if (mohit % 2 == 1) {
-                System.out.println(mohit);
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                    System.out.print(i+ " ");
+                }
+            System.out.println();
             }
         }
     }
-}

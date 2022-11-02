1abd61bf0b12c4feb720c007318bd6955e01a97a
mohittaya67 <Mohittaya67@gmail.com>
2022-11-01 20:49:14 -0400

    newFeature created

--- src/com/pragra/Loops.java
+++ src/com/pragra/Loops.java
@@ -6,7 +6,9 @@ public class Loops {
             for (int j = 1; j <= i; j++) {
                     System.out.print(i+ " ");
                 }
-            System.out.println();
+            System.out.println();git
             }
+        System.out.println("Hello");
         }
+
     }
